7455e9d6171b72387ef26e44e2ba5218fd48916c
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-04-06 06:29:43 +0300

    Example005_Hi

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Hi, my dear!");
+}
+else
+{
+    Console.Write("Good morning, ");
+    Console.WriteLine(username);
+}
