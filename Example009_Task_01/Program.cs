d394121bc518571cc902a297a98f098361a68939
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-03 06:10:25 +0300

    проверка является ли число квадратом другого

---
+++ Example009_Task_01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Insert a number a: ");
+string i = Console.ReadLine();
+int a = Convert.ToInt32(i);
+Console.WriteLine ("Insert a number b: ");
+string j = Console.ReadLine();
+int b = Convert.ToInt32(j);
+
+if (a == b*b)
+{
+ Console.WriteLine ("The number a is a square of the number b");   
+}
+else
+{
+    Console.WriteLine ("The number a IS NOT a square of the number b");
+}
