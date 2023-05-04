4e9025f5a938a309d88d206548404bdfdb921dad
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-04 05:48:30 +0300

    Days of week

---
+++ Example010_Task_02/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine ("Insert a number of the day of the week: ");
+string i = Console.ReadLine();
+int a = Convert.ToInt32(i);
+if (a == 1)
+{
+    Console.WriteLine ("This is Monday!");
+}
+else if (a == 2)
+{
+    Console.WriteLine ("This is Tuesday!");
+}
+else if (a == 3)
+{
+    Console.WriteLine ("This is Wednesday!");
+}
+else if (a == 4)
+{
+    Console.WriteLine ("This is Thursday!");
+}
+else if (a == 5)
+{
+    Console.WriteLine ("This is Friday!");
+}
+else if (a == 6)
+{
+    Console.WriteLine ("This is Saturday!");
+}
+else if (a == 7)
+{
+    Console.WriteLine ("This is Sunday!");
+}
+else
+{
+    Console.WriteLine ("Тhere are only 7 days in a week, try again!");
+}
