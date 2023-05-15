a34d8c924fc7ab1ed20cb90160ddc07c1775a5b2
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-15 05:53:42 +0300

    Ввод трехзначного числа

---
+++ Example013_Task_05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Imput a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a / 100 == 0 || a / 100 > 9)
+{
+    Console.WriteLine("This number is not correct");
+}
+else
+{
+ int i = a % 10;
+Console.WriteLine(i);   
+}
