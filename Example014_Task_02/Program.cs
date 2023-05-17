8f1410c4a55e5b936d1a6a1ef31f2cb96eb54d2f
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-17 06:21:56 +0300

    Определение большего числа

---
+++ Example014_Task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Imput a number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput a number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+Console.Write("max=");
+Console.WriteLine(a);
+}
+else
+{
+Console.Write("max=");
+Console.WriteLine(b);
+}
