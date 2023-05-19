8601642670d17d48cfa3680df85dae60774e1c09
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-19 06:05:53 +0300

    Максимальное из 3 чисел

---
+++ Example015_Task_04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Imput a number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput a number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput a number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a)
+{
+    b = max;
+}
+if (c > max)
+{
+    c = max;
+}
+Console.Write("max=");
+Console.WriteLine(max);
