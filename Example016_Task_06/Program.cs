256d067e3e298ea45ac388e75fc63bffbd77c4f1
VitaliiBrown <vitaliibrown448899@gmail.com>
2023-05-29 07:17:27 +0300

    Even or odd

---
+++ Example016_Task_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Imput a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("This is an even number!");
+}
+else
+{
+    Console.WriteLine("This number is odd!");
+}
